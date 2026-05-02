27491257a54264cc8182bc5c06aece388f560052
ABDULLAH MEWAWALA <abdmw8919@gmail.com>
2026-05-02 21:37:16 +0530

    multilevel inheritance

--- src/oopsConcepts1/InheritanceExp.java
+++ src/oopsConcepts1/InheritanceExp.java
@@ -13,6 +13,8 @@ class AdvCalc extends Calc{
 	//through the "extends" keyword , now the AdvCalc has access to all the methods of Calc
 	//AdvCalc can perform all the operations of Calc
 	//even if nothing is written here , an instance of this class will still be able to perform add and sub operations
+	//therefoe here AdvCalc "is" Calc
+	//at this stage the inheritance is called "single level inheritance"
 	
 	public int multi(int n1, int n2) {
 		return n1*n2;
@@ -22,29 +24,46 @@ class AdvCalc extends Calc{
 		return n1/n2;
 	}
 }
+
+class SciCalc extends AdvCalc{
+	//scientific calc should have all the functions of calc and advcalc
+	//therefore it will extend AdvCalc 
+	//at this stage AdvCalc is inheriting from Calc and SciCalc is inheritiing from AdvCalc
+	//such type of inheritance is called "multilevel inheritance"
+	
+	// it has a new power(n1,n2) operation
+	double power(int n1, int n2) {
+		return Math.pow(n1, n2);
+	}
+	
+}
 public class InheritanceExp {
 	/*
 	 inheritance is a concept where a child class has access to all the properties and methods of
 	 the parent class 
 	 
-	  suppose we want to make 2 different calc 
+	  suppose we want to make 3 different calc 
 	  one basic calc with add() and sub() for 10$
-	  one adv calc with all basic calc function and addtionla multi() and div()
+	  one adv calc with all basic calc function and    multi() and div()
+	  one scientificn calc with all functions of adv calc woth additional power() 
 	 */
 	public static void main(String[] args) {
 		
 		Calc obj = new Calc();
 		AdvCalc aobj = new AdvCalc();
+		SciCalc sobj = new SciCalc();
 		
-		int add = aobj.add(1,2);
-		int sub = aobj.sub(5,4);
-		int multi = aobj.multi(2,3);
-		int div = aobj.div(10,2);
+		int add = sobj.add(1,2);
+		int sub = sobj.sub(5,4);
+		int multi = sobj.multi(2,3);
+		int div = sobj.div(10,2);
+		double power = sobj.power(4,2);
 		
 		System.out.println("addition: "+add);
 		System.out.println("subtraction: "+sub);
 		System.out.println("multiplication: "+ multi);
 		System.out.println("subtraction: "+ div);
+		System.out.println("power: "+ power);
 	}
 	
 }
